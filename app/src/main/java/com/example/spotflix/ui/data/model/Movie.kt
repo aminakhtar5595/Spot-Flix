817f8d0c32c8ca4b21ee0036cf9df6cf6b9b742b
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-04-15 16:44:45 +0500

    Working on API structure

---
+++ app/src/main/java/com/example/spotflix/ui/data/model/Movie.kt
@@ -0,0 +1,9 @@
+package com.example.spotflix.ui.data.model
+
+data class MovieList (
+    val id: Int,
+    val image: String,
+    val title: String,
+    val rating: String,
+    val date: String
+)
---
+++ app/src/main/java/com/example/spotflix/ui/data/model/MovieResponse.kt
@@ -0,0 +1,6 @@
+package com.example.spotflix.ui.data.model
+import com.example.spotflix.ui.viewModels.MovieList
+
+data class MovieResponse (
+    val response: List<MovieList>
+)
