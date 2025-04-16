85879a68bb5ce928d4191bf2054377858ec77fb6
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-04-16 10:27:11 +0500

    Use CenterAlignedTopAppBar instead of box for header in favorite screen

--- app/src/main/java/com/example/spotflix/ui/screens/FavoriteScreen.kt
+++ app/src/main/java/com/example/spotflix/ui/screens/FavoriteScreen.kt
@@ -1,70 +1,64 @@
 package com.example.spotflix.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoriteScreen(navController: NavController) {
     Column {
-        Box(
-            modifier = Modifier
-                .background(color = Color(0xFF4F7CCB))
-                .padding(20.dp)
-                .fillMaxWidth()
-        ) {
-            Icon(
-                imageVector = Icons.Filled.ArrowBack,
-                contentDescription = "Arrow Back",
-                modifier = Modifier
-                    .size(25.dp)
-                    .clickable { navController.popBackStack() },
-                tint = Color.White
-            )
-
-            Text(
-                text = "Your Favorites",
-                style = TextStyle(
-                    fontSize = 20.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.SemiBold,
-                    textAlign = TextAlign.Center,
-                ),
-                modifier = Modifier
-                    .fillMaxWidth(),
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .padding(20.dp)
-        ) {
-            LazyColumn {
-                items(5) {
-                    MovieCard(
-                        title = "Little Man Town",
-                        rating = "5.8",
-                        date = "2022-05-11",
-                        image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaBlEMlTddNboEvHUefVn0RkoJSGa0kvIXNQ&s"
+        CenterAlignedTopAppBar(
+            modifier = Modifier.fillMaxWidth().background(color = Color(0xFF4F7CCB)).padding(horizontal = 15.dp),
+            title = {
+                Text("Your Favorites",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 20.sp,
                     )
-                }
+                )
+            },
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Back",
+                    modifier = Modifier.size(25.dp).clickable { navController.popBackStack() },
+                    tint = Color.White
+                )
+            },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color(0xFF4F7CCB))
+        )
+
+        LazyColumn(
+            contentPadding = PaddingValues(20.dp)
+        ) {
+            items(5) {
+                MovieCard(
+                    title = "Little Man Town",
+                    rating = "5.8",
+                    date = "2022-05-11",
+                    image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaBlEMlTddNboEvHUefVn0RkoJSGa0kvIXNQ&s"
+                )
             }
         }
     }
