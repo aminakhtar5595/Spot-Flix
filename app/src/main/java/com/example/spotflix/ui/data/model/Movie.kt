7505a95fab87f96c5c6c624154f70deecb143177
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-04-16 09:56:32 +0500

    Pushing updated code

--- app/src/main/java/com/example/spotflix/ui/data/model/Movie.kt
+++
@@ -1,9 +0,0 @@
-package com.example.spotflix.ui.data.model
-
-data class MovieList (
-    val id: Int,
-    val image: String,
-    val title: String,
-    val rating: String,
-    val date: String
-)
--- app/src/main/java/com/example/spotflix/ui/data/model/MovieResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.spotflix.ui.data.model
-import com.example.spotflix.ui.viewModels.MovieList
-
-data class MovieResponse (
-    val response: List<MovieList>
-)
