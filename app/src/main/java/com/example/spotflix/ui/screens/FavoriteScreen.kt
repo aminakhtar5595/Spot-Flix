361d57fc87b09f97aa32549a516ddc253dcab586
aminakhtar5595 <aminakhtar5595@gmail.com>
2025-04-16 10:06:37 +0500

    Added dummy data in favorite screen

--- app/src/main/java/com/example/spotflix/ui/screens/FavoriteScreen.kt
+++ app/src/main/java/com/example/spotflix/ui/screens/FavoriteScreen.kt
@@ -1,18 +1,26 @@
 package com.example.spotflix.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -20,6 +28,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 
 @Composable
 fun FavoriteScreen(navController: NavController) {
@@ -50,6 +59,22 @@ fun FavoriteScreen(navController: NavController) {
                 modifier = Modifier
                     .fillMaxWidth(),
             )
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+        ) {
+            LazyColumn {
+                items(5) {
+                    MovieCard(
+                        title = "Little Man Town",
+                        rating = "5.8",
+                        date = "2022-05-11",
+                        image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaBlEMlTddNboEvHUefVn0RkoJSGa0kvIXNQ&s"
+                    )
+                }
+            }
         }
     }
 }
